ac2001269490cc3be1f3c40025e91dec519a47a8
Bowen Zhang <e0555834@u.nus.edu>
2023-10-06 16:37:23 +0800

    add conda setup shell script

---
+++ conda_setup.sh
@@ -0,0 +1,7 @@
+wget https://repo.anaconda.com/miniconda/Miniconda3-latest-Linux-x86_64.sh
+chmod +x Miniconda3-latest-Linux-x86_64.sh
+./Miniconda3-latest-Linux-x86_64.sh -b
+export PATH='/home/ubuntu/miniconda3/bin:$PATH'
+conda init bash
+conda create -n testenv python==3.9.0
+source ~/.bashrc
