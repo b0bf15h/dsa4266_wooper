0a88c39992d9e8fb1600f442959784c8bf4d12a7
Bowen Zhang <e0555834@u.nus.edu>
2023-10-11 00:38:28 +0800

    update requirements and train_test

--- TrainTestSplit.py
+++ TrainTestSplit.py
@@ -19,6 +19,8 @@ class TrainTestSplit(object):
       train_data=self.merged_pkl_data[self.merged_pkl_data['gene_id'].isin(train_gene_id)==True]
       test_data=self.merged_pkl_data[self.merged_pkl_data['gene_id'].isin(train_gene_id)!=True]
       print("TRAIN-TEST SPLIT SUCCESSFUL")
+      train_data.drop(['transcript_id', 'gene_id'], axis = 1, inplace=True)
+      test_data.drop(['transcript_id', 'gene_id'], axis = 1, inplace=True)
       train_data.to_pickle(data_path/'train_data.pkl')
       test_data.to_pickle(data_path/'test_data.pkl')
       return (train_data,test_data)
