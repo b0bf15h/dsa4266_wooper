d59e4e48b839ebc4c4cc63b9699ae0f2173bd112
Bowen Zhang <e0555834@u.nus.edu>
2023-11-11 19:18:12 +0800

    update predictions.sh

--- predictions.sh
+++ predictions.sh
@@ -2,3 +2,8 @@ mkdir ./data/prediction_data
 ./pull_data.sh
 ./process_inference_data.sh 
 python main.py -d ./data -s 3 -m ./models -mn 'model.pkl' -dn 'prediction_data.pkl'
+cd data
+rm biomart_data.csv prediction_data_ids_and_positions.pkl prediction_data.pkl
+cd prediction_data
+rm data.json
+cd ../..
