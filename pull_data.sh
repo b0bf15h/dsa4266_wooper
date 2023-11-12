e2702bea0e2c6d74a50f5071a85d18674d5b049f
Bowen Zhang <e0555834@u.nus.edu>
2023-11-12 22:34:41 +0800

    update pull data

--- pull_data.sh
+++ pull_data.sh
@@ -1,8 +1,8 @@
 mkdir ./data/prediction_data
 mkdir ./models
-ds0=''
+ds0='12FGrDUy6hWm8B3U3nbZxFxIXIAy3XMrZ'
 ds0_name='dataset0.json.gz'
-ds0_info=''
+ds0_info='10KB36iB1lY_jybFwzrDfQTVI6vrUwpBb'
 ds0_infon='data.info'
 ds1='1BhgOM9UfJKYyjYSaZxBMOhLpB8SYiP97' 
 ds1_name='dataset1.json.gz'
@@ -16,14 +16,16 @@ rf='1yTfHRyuF-wupwZxSlb1XIajMBktRr5SR'
 rf_name='rf_final_model.pkl'
 rf_nrp='1CvUbJ5YvfezE8Hz-38h-VAiMPjqZv6ZH'
 rf_nrp_name='rf_no_rp_final_model.pkl'
+gdown "https://drive.google.com/uc?id=$ds0_info" -O "$ds0_infon"
+gdown "https://drive.google.com/uc?id=$ds0" -O "$ds0_name"
 gdown "https://drive.google.com/uc?id=$ds1" -O "$ds1_name"
 gdown "https://drive.google.com/uc?id=$ds2" -O "$ds2_name"
 gdown "https://drive.google.com/uc?id=$ds3" -O "$ds3_name"
 gdown "https://drive.google.com/uc?id=$ds3_csv" -O "$ds3_csv_name"
 gdown "https://drive.google.com/uc?id=$rf" -O "$rf_name"
 gdown "https://drive.google.com/uc?id=$rf_nrp" -O "$rf_nrp_name"
-mv dataset0.json.gz ./data/
 mv data.info ./data/
+mv dataset0.json.gz ./data/
 mv dataset1.json.gz ./data/
 mv dataset2.json.gz ./data/
 mv dataset3.json.gz ./data/
